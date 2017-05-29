07066973a243b52256e6e31c5576620a6838431f
liangshangyu <2460641129@qq.com>
2017-05-29 16:34:11 +0800

    创建服务并显示页面

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/5/29.
+ */
+let express = require('express');
+let path = require('path')
+let Movie = require('./model').Movie;
+let app = express();
+app.set('view engine','ejs')
+app.use(express.static(path.resolve('node_modules')))
+app.get('/',function (req,res) {
+    Movie.find({},function (err,movies) {
+        res.render('index',{movies})
+    })
+})
+app.listen(8030);
